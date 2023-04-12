3d0cd433b1b32175c9a4a85b8867db5dac015c51
Mordecai <mordecai.mengesteab@gmail.com>
2023-04-12 13:22:26 -0400

    Added random color functionality to all 3 colors buttons

--- guess-the-color-game/script.js
+++ guess-the-color-game/script.js
@@ -14,10 +14,15 @@ function randomColor() {
 let targetColor = randomColor();
 let correctBlock = Math.floor(Math.random()*3);
 
+
+//*
 for(let i = 0; i < 3; i++) {
   let tempRandom = randomColor();
-  colors[i].styles.backgroundColor = `rgb(${tempRandom[0]}, ${tempRandom[1]}, ${tempRandom[2]})`
+  colors[i].style.backgroundColor = `rgb(${tempRandom[0]}, ${tempRandom[1]}, ${tempRandom[2]})`
 }
+ //*/
+
+
 
 colors[correctBlock].style.backgroundColor = `rgb(${targetColor[0]}, ${targetColor[1]}, ${targetColor[2]})`
 
